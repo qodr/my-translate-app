415a0199a27ae88fcff9dfde82860806697a1226
Hakan Yildirim <hakan.yildirim@abnamroabf.com>
2020-02-13 15:10:07 +0100

    i18n applied to datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DateAdapter } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-translate-app';
+
+  constructor(private dateAdapter: DateAdapter<AppComponent>) {
+
+  }
+
+  setLocale(locale: string) {
+    this.dateAdapter.setLocale(locale);
+  }
 }
