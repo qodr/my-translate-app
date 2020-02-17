07e09f5b80365b9f146bc45003c3004ca3b58918
Hakan Yildirim <hakan.yildirim@abnamroabf.com>
2020-02-17 12:16:40 +0100

    added plural and select examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule, MatNativeDateModule, MatDatepickerModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatFormFieldModule, MatNativeDateModule, MatDatepickerModule, MatInputModule, MatSelectModule, MatButton, MatButtonModule, MatRadioModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader, TranslateCompiler } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateMessageFormatCompiler, MESSAGE_FORMAT_CONFIG } from 'ngx-translate-messageformat-compiler';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -31,17 +32,25 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelectModule,
+    MatButtonModule,
+    MatRadioModule,
     TranslateModule.forRoot({
       loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
           deps: [HttpClient]
       },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler
+      },
       defaultLanguage: 'en'
     }),
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    // { provide: MESSAGE_FORMAT_CONFIG, useValue: { locales: ['en', 'nl', 'tr'] }}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
